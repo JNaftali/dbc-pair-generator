7418b9c7a1c16bcac94c00ffcd6bcc58c28fc9fb
JNaftali <marantzj@gmail.com>
2016-08-17 19:21:22 -0400

    working prototype

--- pair_generator.rb
+++ pair_generator.rb
@@ -1,18 +1,27 @@
 module PairGenerator
-  def find_new_pairs(history)
-    people = history.flatten.unique
+  def self.find_new_pairs(history)
+    people = history.flatten.uniq
     possible_pairs = people.combination(2).to_a - history
 
     get_pairs(people, possible_pairs)
   end
 
   private
-  def get_pairs(people, possible_pairs)
+  def self.get_pairs(people, possible_pairs)
+    # binding.pry
+    return [] if people.length == 0
+    return [people] if people.length == 1
     possible_pairs.each do |pair_candidate|
       safe_possible_pairs = possible_pairs.dup
       safe_people = people.dup - pair_candidate
       remaining_pairs = safe_possible_pairs.delete_if {|pair| pair.include?(pair_candidate[0]) || pair.include?(pair_candidate[1])}
-      return [pair_candidate] + get_pairs(safe_people, remaining_pairs) if people.all? {|person| remaining_pairs.flatten.include?(person)}
+      if safe_people.all? {|person| remaining_pairs.flatten.include?(person)}
+        if other_pairs = get_pairs(safe_people, remaining_pairs)
+          # binding.pry
+          return other_pairs + [pair_candidate]
+        end
+      end
     end
+    false
   end
 end
