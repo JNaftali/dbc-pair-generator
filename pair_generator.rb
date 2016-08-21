841924de67a732a782695f5e4bf33fa9203a9b43
Joshua Marantz <email": null>
2016-08-21 19:51:45 -0400

    Well I suppose I've been making progress...

--- pair_generator.rb
+++ pair_generator.rb
@@ -1,15 +1,32 @@
+require 'forwardable'
+
 class PairGenerator
   attr_reader :people, :history
-  def initialize(array)
-    if array.any? {|el| el.is_a?(Array)}
-      @history = array
-    else
-      @people = array
-    end
-    @history ||= []
-    @people ||= []
-    @people += @history.flatten.uniq
+  def initialize(args={})
+    @history = args.fetch(:history, []).map(&:to_a)
+    @people = args.fetch(:people, []) | @history.flatten
     @history.map! {|pair| Pair.new(pair)}
+  end
+
+  def valid?
+    # return false if self.permitted_pairs.length == 0 && self.people.length > 0
+    self.people.all? { |person| self.permitted_pairs.any?{ |pair| pair.include?(person) } }
+  end
+
+  def get_next_pairs
+    return [self.people[0]] if self.people.length == 1
+    return [self.permitted_pairs[0]] if self.people.length == 2 && self.permitted_pairs.length == 1
+    self.permitted_pairs.each do |next_pair|
+      # binding.pry
+      next_history = self.history.reject { |past_pair| past_pair.overlap?(next_pair) }
+      next_people = self.people - next_pair.people
+      next_group = PairGenerator.new(history: next_history, people: next_people)
+      next unless next_group.valid?
+      pairs = next_group.get_next_pairs
+      next unless pairs
+      return [next_pair] + next_group.get_next_pairs
+    end
+    false
   end
 
   def possible_pairs
@@ -19,31 +36,34 @@ class PairGenerator
   def permitted_pairs
     self.possible_pairs.reject { |pair| @history.any?{ |past_pair| pair == past_pair}}
   end
-
-  def get_pairs
-    return [] if people.length == 0
-    return [people] if people.length == 1
-    return false unless people.all? {|person| possible_pairs.flatten.include?(person)}
-  end
 end
 
 class Pair
   attr_reader :people
+  include Enumerable
   include Comparable
+  extend Forwardable
 
-  def initialize(array)
-    @people = array
+  def_delegators :@people, :each, :include?
+
+  def initialize(*array)
+    @people = array.flatten.sort
   end
 
   def <=>(other)
-    self.people.sort <=> other.people.sort
+    self.people <=> other.people
   end
 
-  def include?(thing)
-    self.people.include?(thing)
+  def overlap?(other_pair)
+    @people.any? {|person| other_pair.include?(person)}
+  end
+
+  def to_a
+    @people
   end
 end
 
+# test = PairGenerator.new(history: [[:jon, :melissa], [:josh, :shawn], [:anders, :terrance], [:ephraim, :kiren], [:jon, :amanda], [:anders, :shawn], [:josh, :terrance], [:melissa, :kiren], [:amanda, :neel], [:edwin, :jon], [:ephraim, :shawn], [:henri, :rachel], [:josh, :melissa], [:terrance, :kiren], [:amanda, :ephraim], [:anders, :rachel], [:daniel, :josh], [:neel, :terrance], [:moin, :melissa]])
 # past_pairs = [[:jon, :melissa], [:josh, :shawn], [:anders, :terrance], [:ephraim, :kiren], [:jon, :amanda], [:anders, :shawn], [:josh, :terrance], [:melissa, :kiren], [:amanda, :neel], [:edwin, :jon], [:ephraim, :shawn], [:henri, :rachel], [:josh, :melissa], [:terrance, :kiren], [:amanda, :ephraim], [:anders, :rachel], [:daniel, :josh], [:neel, :terrance], [:moin, :melissa]]
 
 # p PairGenerator.find_new_pairs(past_pairs)
