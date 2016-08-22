ad586f3b8fb58569bb80fb2557ea0e66ef857bba
Joshua Marantz <email": null>
2016-08-21 22:12:52 -0400

    Added runner which returns all combinations of the given group of people... I think

---
+++ runner.rb
@@ -0,0 +1,14 @@
+require_relative 'pair_generator'
+
+roster = [:jon, :melissa, :josh, :shawn, :anders, :terrance, :ephraim, :kiren, :amanda, :neel, :edwin, :henri, :rachel, :daniel, :moin]
+pair_groups = []
+
+history = {people: roster}
+
+while new_group = PairGenerator.new(history).next_set_of_pairs
+  pair_groups << new_group
+  history = {history: pair_groups.flatten(1)}
+end
+pair_groups.each do |pair_group|
+  puts pair_group.reduce('| ') { |str, pair| str + pair.join(", ") + " | " }
+end
