8ae49ceaf2c8cca290bf95403a7b5c7473a36621
Joshua Marantz <email": null>
2016-08-28 16:33:15 -0400

    Working parser and example text file

---
+++ parser.rb
@@ -0,0 +1,23 @@
+module PairParser
+  def self.load_file(path)
+    file = File.new(path)
+    people = []
+    until (next_person = file.gets) == "\n"
+      people << next_person.chomp.to_sym
+    end
+    history = []
+    while next_group = file.gets
+      history << next_group.chomp.split(/, ?/).map(&:to_sym)
+    end
+    file.close
+    PairGenerator.new({people: people, history: history})
+  end
+
+  def self.save_file(path, pairgen)
+    file = File.open(path, "w") do |f|
+      pairgen.people.each { |person| f << person << "\n" }
+      f << "\n"
+      pairgen.history.each { |group| f << group.map(&:to_s).join(", ") << "\n" }
+    end
+  end
+end
